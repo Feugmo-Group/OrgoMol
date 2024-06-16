bc398c53599877c3a22cfeeebbdd688fc42395ed
Mafuoso <77584862+Mafuoso@users.noreply.github.com>
2024-06-15 21:08:42 -0400

    Progess on sanityCheck, semi-functional but currently permutation variant. Working on writing function to add validation status to a file

--- testEnv/sanityCheck.py
+++ testEnv/sanityCheck.py
@@ -1,5 +1,8 @@
 import gcutil
 import numpy as np
+from scipy.spatial.distance import cdist
+from p_tqdm import p_map
+import csv
 
 #goal is to convert zmat back to xyz
 #then check whether the new xyz files are the same as the old xyz files
@@ -10,6 +13,17 @@ import numpy as np
 #as of now when I am doing the RMSD I am assuming that both matrices have the same labels for each entry ie poisiton 1,1 corresponds to the same atom for both which may be eggregious
 #also assuming matrixes are same shape
 
+#No point in doing rMSD and I might delete the implementation if I check it and find out it's wrong because a better metric that is permutation variant is the distance matrix for which
+#i already have a function
+
+#Current way of comparing. Compute distance matrix for both xyz files. Then using Frobenius Norm to see if they are the same or not
+#can also use correlation coefficent or compare entry wise
+
+#SHOWING POSITIVE RESULTS!! Matrixes have same Frobenius norm. Now for future datasets, not this one because this one the conversion conserves the permutation, but for future need to allign
+#the permutation before computing norm
+
+
+
 def getArgs(zmatInput:str,xyzInput:str) -> None:
     with open(zmatInput, "r"), open(xyzInput,"r"):
         zmatAtomNames, rconnect, rlist, aconnect, alist, dconnect, dlist = gcutil.readzmat(zmatInput)
@@ -17,7 +31,7 @@ def getArgs(zmatInput:str,xyzInput:str) -> None:
         zarr =  gcutil.write_xyz(zmatAtomNames,rconnect,rlist,aconnect,alist,dconnect,dlist)
         return zarr, xyzarr
             
-def inner(mat1,mat2)-> int:
+def matrixInner(mat1,mat2)-> int:
     mat2T = np.transpose(mat2)
     return np.trace(np.matmul(mat2T,mat1))
 
@@ -36,10 +50,31 @@ def rMSD(mat1:np.ndarray,mat2:np.ndarray,numOfAtoms:int) -> float:
     array1, array2 = np.array(mat1Reduced), np.array(mat2Reduced)
     rMSD = np.sqrt((sum((array1-array2)**2))*(1/numOfAtoms))
     return rMSD
-      
-zarr,xyzarr =  getArgs("dsgdb9nsd_000001.zmat", "dsgdb9nsd_000001.xyz") 
-print(rMSD(zarr,xyzarr,5))    
-  
-    
 
-       
+def distanceMatrix(arg1:np.ndarray, arg2:np.ndarray) -> np.ndarray:
+    return cdist(arg1,arg2)
+    
+def compare(file1:str,file2:str) -> bool:      
+    zarr,xyzarr =  getArgs(file1,file2)
+    zDMatrix = distanceMatrix(zarr,zarr)
+    xDMatrix = distanceMatrix(xyzarr,xyzarr)
+    zNorm = np.linalg.norm(zDMatrix)
+    xNorm = np.linalg.norm(xDMatrix)
+    if (zNorm-xNorm)**2 < 0.01:
+      return True
+    else:
+        return False
+    
+    
+#now just need a way to compare 128,744 files quickly! and store the results somewhere
+#current validation function is not really paralllelizable, not sure how much of an issue this is considering that you would not run this calculation many times
+
+def validation(dataSet:str) -> None:
+      with open(dataSet, "r", newline = '') as myfile:
+            data = csv.reader(myfile)
+            for row in data:
+                print(row)
+                #row.append(str(compare(zFile,xyzFile)))
+                #print(zFile,xyzFile)
+                
+validation("testSet.csv")
