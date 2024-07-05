f80f45ce51bff4bbc9cd1e06687b9b0a547ad08b
Mohid <mohidfarooqi2005@gmail.com>
2024-07-04 22:40:07 -0400

    getProperties newest version

--- Utillities/sampler.py
+++ Utillities/sampler.py
@@ -15,14 +15,14 @@ def sciRandomSample(file:str) -> pd.DataFrame:
     with open(file,"r") as myfile:
         rawData = pd.read_csv(myfile)
         filterData = rawData.query("status == True", inplace=False)
-        train,test = train_test_split(filterData,train_size=10000, test_size= 2000)
+        train,test = train_test_split(filterData,train_size=50000, test_size= 10000)
         return train,test
     
 trainingData, testData = sciRandomSample("f(Validated)trainingSet.csv")
 
-trainingData.to_csv("trainingSet(10000).csv",sep=",")
+trainingData.to_csv("trainingSet(50000).csv",sep=",")
 
-testData, validationData = train_test_split(testData, train_size=1600,test_size=400)
+testData, validationData = train_test_split(testData, train_size=8000,test_size=2000)
 
-testData.to_csv("testSet(1600).csv", sep = ",")
-validationData.to_csv("validationSet(400).csv", sep= ",")
+testData.to_csv("testSet(8000).csv", sep = ",")
+validationData.to_csv("validationSet(2000).csv", sep= ",")
